6882940d739732a69efa474de33eb34e9d74c050
divyanshu <kdivyanshu99@gmail.com>
2021-02-12 14:19:59 +0530

    validation

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -11,8 +11,12 @@ export class RegisterComponent implements OnInit {
   constructor() { }
  
   form_el=[{'item1':'','item2':'','item3':'','item4':'','item5':'','item6':'','item7':'','item8':'','item9':'','item10':'','item11':''}]
- 
+  //stat=[{'st1':'Himachal Pradesh','st2':'Haryana','st3':'Uttarakhand','st4':'Uttar Pradesh','st5':'Rajasthan'}]
   ngOnInit(): void {
+  }
+  clear()
+  {
+    this.form_el=[{'item1':'','item2':'','item3':'','item4':'','item5':'','item6':'','item7':'','item8':'','item9':'','item10':'','item11':''}]
   }
   signupUser(signupForm:NgForm)
   {
